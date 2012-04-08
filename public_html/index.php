98e1dedaa078efc214d88d9ac904d76fe6d02197
Niklas Schönberg <niko@uplink.fi>
2012-04-08 12:12:23 +0300

    Initial code commit. Needs a LOT of cleanup work.

---
+++ public_html/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require("../globals/init.inc");
+require("../globals/init_heraldry.inc");
+
+include("../headers/header.inc");
+include("headers/header.inc");
+
+include("templates/$_GET[template].inc");
+
+include("headers/footer.inc");
+include("../headers/footer.inc");
+
+?>
