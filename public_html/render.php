ea23f31132e853280cf6fe298ace77957ddacc31
yaleman <yaleman@ricetek.net>
2012-04-11 17:25:30 +1000

    Clean up output size handling

--- public_html/render.php
+++ public_html/render.php
@@ -17,21 +17,20 @@ else
 $svg = '<?xml version="1.0" encoding="UTF-8"?>\n';
 $hash = md5($_SERVER["QUERY_STRING"]);
 
+$size = isset( $_GET[size] ) ? $_GET[size] : 100;
+
 switch ($_GET[type]) {
 	case "charge":
 		$iname = "charge";
-		if (!isset($_GET[size]))
-			$_GET[size] = 100;
+		
 		$svg .= renderCharge($_GET[id]);
 		$f = strpos($svg, "100%");
-		$svg = substr_replace($svg, $_GET[size]."px", $f, 4);
+		$svg = substr_replace($svg, $size."px", $f, 4);
 		$f = strpos($svg, "100%");
-		$svg = substr_replace($svg, $_GET[size]."px", $f, 4);
+		$svg = substr_replace($svg, $size."px", $f, 4);
 	break;
 	case "shield":
 		$iname = "shield";
-		if (!isset($_GET[size]))
-			$_GET[size] = 100;
 		if (isset($_GET[ord])) {
 			if (is_array($_GET[ord])) {
 				$ord = $_GET[ord];
@@ -44,9 +43,9 @@ switch ($_GET[type]) {
 		} 
 		$svg .= renderShield ($_GET[fld],$_GET[div],"",$_GET[dcl1],$_GET[dcl2],$ord);
 		$f = strpos($svg, "100%");
-		$svg = substr_replace($svg, $_GET[size]."px", $f, 4);
+		$svg = substr_replace($svg, $size."px", $f, 4);
 		$f = strpos($svg, "100%");
-		$svg = substr_replace($svg, $_GET[size]."px", $f, 4);
+		$svg = substr_replace($svg, $size."px", $f, 4);
 	break;
 	case "coa":
 		$iname = "coat of arms";
@@ -74,8 +73,7 @@ switch ($output_format) {
 	break;
 	case "png":
 		if (!$fmt) $fmt = "image/png";
-		if (isset($_GET[size])) {
-			$extopt = " -a -h $_GET[size]";
+		$extopt = " -a -h {$size}";
 		}
 		// Passtrough
 	case "pdf":
@@ -89,7 +87,7 @@ switch ($output_format) {
 			header("Pragma: public");
 			header("Expires: ".date("D, d M Y H:i:s T",time()+86400));
 			header("Content-Disposition: inline; filename=".$iname.".".$_GET[format]);
-			passthru("rsvg-convert$extopt --format=$_GET[format] $file");
+			passthru("rsvg-convert{$extopt} --format={$_GET[format]} {$file"});
 			unset($file);
 		}
 	break;
