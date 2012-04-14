72758b09d81ce271b7806859b38e6e9a1a7fa0cf
yaleman <yaleman@ricetek.net>
2012-04-14 20:50:05 +1000

    Cleaned up relative paths

--- public_html/index.php
+++ public_html/index.php
@@ -1,14 +1,14 @@
 <?php
 
-require("../globals/init.inc");
-require("../globals/init_heraldry.inc");
+// start building the page
+require("globals/init.inc");
 
-include("../headers/header.inc");
+require("globals/init_heraldry.inc");
+
 include("headers/header.inc");
 
 include("templates/$_GET[template].inc");
 
 include("headers/footer.inc");
-include("../headers/footer.inc");
 
 ?>
