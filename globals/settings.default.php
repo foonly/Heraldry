336ac4c63a09cdeeebc7578580916fc2bb405d6f
Niklas Schönberg <niko@uplink.fi>
2014-05-13 21:25:00 +0300

    Moar cleanup.

--- globals/settings.default.php
+++ globals/settings.default.php
@@ -3,24 +3,24 @@
 // Copy this file to settings.inc on deployment.
 
 // Database Settings
-$setting['db'][type] = "pgsql"; // mysql or pgsql
-$setting['db'][server] = ""; // hostname or ip
-$setting['db'][port] = 5432; // 3306 for mysql & 5432 for postgresql
-$setting['db'][user] = "";
-$setting['db'][password] = "";
-$setting['db'][name] = "";
+$setting['db']['type'] = "pgsql"; // mysql or pgsql
+$setting['db']['server'] = ""; // hostname or ip
+$setting['db']['port'] = 5432; // 3306 for mysql & 5432 for postgresql
+$setting['db']['user'] = "";
+$setting['db']['password'] = "";
+$setting['db']['name'] = "";
 
-$setting[schema]="public";
-$setting[site_uri]="";
-$setting[title]="Uplink Heraldry Site";
-$setting[rpath]="";
-$setting[apath]="";
-$setting[tmppath]="";
-$setting[imgpath]="";
-$setting[imgcache]="";
-$setting[svgbase]=$setting[apath]."/svg_base";
-$setting[dev] = 1;
-$setting[css] = array("base");
+$setting['schema']="public";
+$setting['site_uri']="";
+$setting['title']="Uplink Heraldry Site";
+$setting['rpath']="";
+$setting['apath']="";
+$setting['tmppath']="";
+$setting['imgpath']="";
+$setting['imgcache']="";
+$setting['svgbase']=$setting[apath]."/svg_base";
+$setting['dev'] = 1;
+$setting['css'] = array("base");
 
 $locale="en";
 
