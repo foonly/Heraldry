b5af29da1dfb0c70222c30a07a13d5ba04703dd5
Tommy Schonberg <tommy@scarab.uplink.fi>
2014-05-16 22:13:10 +0300

    added styles in css

---
+++ public_html/transaction.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+require "../include/init.php";
+
+
+
+$sOutput = "";
+$fullpath = "../transaction/{$t}.inc";
+if (file_exists($fullpath)) {
+    // Save all output from called php template
+    ob_start();
+    include $fullpath;
+    $sOutput = ob_get_contents();
+    ob_clean();
+}
+/*
+// Assign output to smarty
+$smarty->assign("scriptoutput",$sOutput);
+*/
+include("../transaction/$_GET[t].inc");
+
+
+?>
