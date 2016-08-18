ddbbb37fa03975949830667a13d2361c3eabe3be
Niklas Schönberg <niklas@uplink.fi>
2016-08-18 20:48:41 +0300

    Fixed some small issues.

--- public/index.php
+++ public/index.php
@@ -6,14 +6,16 @@ require_once '../init/initView.php';
 /* menu array */
 $menu = array(array('href' => 'news', 'name' => 'News'),array('href' => 'link2', 'name' => 'Menu 2'),array('href' => 'link3', 'name' => 'Menu 3'));
 /*check for template*/
-$template = $_GET['template'];
-if (!$template) {
+if (empty($_GET['template'])) {
 	$template = $menu[0]['href'];
+} else {
+    $template = $_GET['template'];
 }
 /*check for section*/
-$section = $_GET['section'];
-if (!$section) {
+if (empty($_GET['section'])) {
 	$section = 'main';
+} else {
+    $section = $_GET['section'];
 }
 
 
@@ -22,7 +24,7 @@ require_once '../controller/main.php';
 
 /*combine common vars and page specific vars*/
 $common_var = array('tmenu' => $menu, 'selected' => $template);
-$page_var = array('name1' => 'Tommy', 'userc' => $user, 'data' => $data);
+$page_var = array('name1' => 'Tommy', 'userc' => $user);
 $twig_var = $common_var + $page_var;
 
 echo $twig->render($section.'/'.$template.'.twig', $twig_var);
