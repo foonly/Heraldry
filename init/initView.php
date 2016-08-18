3c4ce3c8ce42a6f24e5ce0ac02cc8d1a02647fbd
Niklas Schönberg <niklas@uplink.fi>
2016-08-18 18:20:15 +0300

    Updated path.

--- init/initView.php
+++ init/initView.php
@@ -7,7 +7,7 @@ Twig_Autoloader::register();
 /*auto_reload value to refresh cache*/
 $loader = new Twig_Loader_Filesystem('../templates');
 $twig = new Twig_Environment($loader, array(
-    'cache' => '../cache',
+    'cache' => $setting['apath'].'/cache',
     'auto_reload' => true
 ));
 /* finished loading twig*/
